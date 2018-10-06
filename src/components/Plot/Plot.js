c5cc8520d671615a97b98b48679a6644e15ee77d
Jeremy Kahn <jeremyckahn@gmail.com>
2018-10-06 12:51:35 -0500

    Style field as a grid

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './Plot.css';
 
+import pixel from '../../img/pixel.png';
+
 class Plot extends Component {
   state = {};
 
   render() {
-    return <div className="Plot" />;
+    return (
+      <div className="Plot">
+        <img src={pixel} alt="TODO: Place explanatory text here" />
+      </div>
+    );
   }
 }
 
