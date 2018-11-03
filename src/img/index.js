3adb7115a7554398b57006cfcf3b71a88681cb35
Jeremy Kahn <jeremyckahn@gmail.com>
2018-11-03 11:53:03 -0500

    Index additional carrot and pumpkin life stage images

--- src/img/index.js
+++ src/img/index.js
@@ -1,7 +1,12 @@
-import carrotSeedsImage from '../img/field-tiles/carrot-seeds.png';
-import pumpkinSeedsImage from '../img/field-tiles/pumpkin-seeds.png';
-
 export const items = {
-  'carrot-seeds': carrotSeedsImage,
-  'pumpkin-seeds': pumpkinSeedsImage,
+  // Crops
+  'carrot-dead': require('../img/crops/carrot-dead.png'),
+  'carrot-growing': require('../img/crops/carrot-growing.png'),
+  'carrot-grown': require('../img/crops/carrot-grown.png'),
+  'carrot-seeds': require('../img/crops/carrot-seeds.png'),
+
+  'pumpkin-dead': require('../img/crops/pumpkin-dead.png'),
+  'pumpkin-growing': require('../img/crops/pumpkin-growing.png'),
+  'pumpkin-grown': require('../img/crops/pumpkin-grown.png'),
+  'pumpkin-seeds': require('../img/crops/pumpkin-seeds.png'),
 };
