a98d666be9e7877b4e71b78db1b59dbe6b7b9d87
Jeremy Kahn <jeremyckahn@gmail.com>
2019-02-21 08:13:07 -0600

    Remove cropType property from mock items that are not crops

--- src/data/__mocks__/items.js
+++ src/data/__mocks__/items.js
@@ -8,19 +8,16 @@ const { SEED, GROWING } = cropLifeStage;
 // Non-crop items
 export const sampleItem1 = testItem({
   id: 'sample-item-1',
-  cropType: 'SAMPLE-ITEM-TYPE-1',
   value: 1,
 });
 
 export const sampleItem2 = testItem({
   id: 'sample-item-2',
-  cropType: 'SAMPLE-ITEM-TYPE-2',
   value: 2,
 });
 
 export const sampleItem3 = testItem({
   id: 'sample-item-3',
-  cropType: 'SAMPLE-ITEM-TYPE-3',
   value: 3,
 });
 
