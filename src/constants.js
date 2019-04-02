72a38fa46e96ff21fdcac2d82b15e297a5ecb729
Jeremy Kahn <jeremyckahn@gmail.com>
2019-04-02 08:00:22 -0500

    Reduce RAIN_CHANCE

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const FERTILIZER_ITEM_ID = 'fertilizer';
 export const INITIAL_FIELD_WIDTH = 6;
 export const INITIAL_FIELD_HEIGHT = 10;
 
-export const RAIN_CHANCE = 0.3;
+export const RAIN_CHANCE = 0.1;
 
 export const SPRINKLER_ITEM_ID = 'sprinkler';
 export const SPRINKLER_RANGE = 1;
