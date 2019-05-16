93cef52210aa85f3422f4394a28d22252126c963
Jeremy Kahn <jeremyckahn@gmail.com>
2019-05-16 07:58:40 -0500

    Fix debug menu transition

--- src/mui-theme.js
+++ src/mui-theme.js
@@ -1,10 +1,6 @@
 import { createMuiTheme } from '@material-ui/core';
 
 export default createMuiTheme({
-  transitions: {
-    // https://material-ui.com/getting-started/faq/#how-can-i-disable-animations-globally
-    create: () => '',
-  },
   typography: {
     // Needed to silence this warning:
     // https://material-ui.com/style/typography/#migration-to-typography-v2
