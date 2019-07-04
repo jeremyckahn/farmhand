5170f865fddda346e6514438e75ab3076c61a05c
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-04 12:44:19 -0500

    Render cow gender indicators

--- src/components/CowPenContextMenu/CowPenContextMenu.js
+++ src/components/CowPenContextMenu/CowPenContextMenu.js
@@ -3,10 +3,18 @@ import { object } from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
+import Icon from '@material-ui/core/Icon';
 
 import { animals } from '../../img';
 import FarmhandContext from '../../Farmhand.context';
+import { genders } from '../../enums';
 import './CowPenContextMenu.sass';
+
+const genderIcons = {
+  [genders.MALE]: <Icon className="fa fa-mars" />,
+  [genders.FEMALE]: <Icon className="fa fa-venus" />,
+  [genders.NONBINARY]: <Icon className="fa fa-genderless" />,
+};
 
 export const CowPenContextMenu = ({ cowForSale }) => (
   <div className="CowPenContextMenu">
@@ -15,7 +23,11 @@ export const CowPenContextMenu = ({ cowForSale }) => (
       <CardHeader
         {...{
           avatar: <img {...{ src: animals.cow.white }} alt="Cow" />,
-          title: cowForSale.name,
+          title: (
+            <p>
+              {cowForSale.name} {genderIcons[cowForSale.gender]}
+            </p>
+          ),
           subheader: <p>{cowForSale.weight} lbs.</p>,
         }}
       />
