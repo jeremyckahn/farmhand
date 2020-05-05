bafd04a30a229935da9303b1404aceb68d713108
Jeremy Kahn <jeremyckahn@gmail.com>
2020-05-05 08:48:48 -0500

    Add home screen welcome message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 import FarmhandContext from '../../Farmhand.context'
 
@@ -6,7 +7,20 @@ import './Home.sass'
 
 const Home = () => (
   <div className="Home">
-    <h1>Welcome!</h1>
+    <ReactMarkdown
+      {...{
+        linkTarget: '_blank',
+        source: `
+# Welcome!
+
+You're playing Farmhand, an open source game being developed by [Jeremy Kahn](https://github.com/jeremyckahn). The game is currently smack dab in the middle of development, so it is incomplete and very unpolished. However, you're welcome to try out what's already here!
+
+This is a resource management game that puts a farm in your hand. It is designed to be mobile-friendly and fun for 30 seconds or 30 minutes at a time. Give it a try!
+
+If you'd like to follow this project's development, please check it out on [GitHub](https://github.com/jeremyckahn/farmhand)! 🐮
+    `,
+      }}
+    />
   </div>
 )
 
