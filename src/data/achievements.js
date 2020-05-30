01efa103b11cfb871812091e40a2c6ca6f1168e2
Jeremy Kahn <jeremyckahn@gmail.com>
2020-05-30 15:24:24 -0500

    Implement water-crop achievement

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -4,29 +4,27 @@ import { cropLifeStage } from '../enums'
 
 const { SEED } = cropLifeStage
 
+const addMoney = (state, reward) => ({ ...state, money: state.money + reward })
+
 export default [
-  {
-    ...((reward = 150) => ({
-      id: 'plant-crop',
-      name: 'Plant a crop',
-      description: 'Purchase a seed and plant it in the field.',
-      rewardDescription: moneyString(reward),
-      condition: state =>
-        findInField(
-          state.field,
-          plot => plot && getCropLifeStage(plot) === SEED
-        ),
-      reward: state => ({ ...state, money: state.money + reward }),
-    }))(),
-  },
-  {
+  ((reward = 150) => ({
+    id: 'plant-crop',
+    name: 'Plant a crop',
+    description: 'Purchase a seed and plant it in the field.',
+    rewardDescription: moneyString(reward),
+    condition: state =>
+      findInField(state.field, plot => plot && getCropLifeStage(plot) === SEED),
+    reward: state => addMoney(state, reward),
+  }))(),
+  ((reward = 150) => ({
     id: 'water-crop',
     name: 'Water a crop',
     description: 'Water a crop that you planted.',
-    rewardDescription: moneyString(150),
-    condition: () => false,
-    reward: state => state,
-  },
+    rewardDescription: moneyString(reward),
+    condition: state =>
+      findInField(state.field, plot => plot && plot.wasWateredToday),
+    reward: state => addMoney(state, reward),
+  }))(),
   {
     id: 'harvest-crop',
     name: 'Harvest a crop',
