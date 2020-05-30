4345bb9dbd18854567dde90c4e4f2ce675d763e3
Jeremy Kahn <jeremyckahn@gmail.com>
2020-05-30 15:12:47 -0500

    Partition complete and incomplete achievements

--- src/components/AchievementsView/AchievementsView.js
+++ src/components/AchievementsView/AchievementsView.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { object } from 'prop-types'
+import memoize from 'fast-memoize'
+import Divider from '@material-ui/core/Divider'
 
 import FarmhandContext from '../../Farmhand.context'
 import Achievement from '../Achievement'
@@ -6,19 +9,51 @@ import achievements from '../../data/achievements'
 
 import './AchievementsView.sass'
 
-const AchievementsView = () => (
+const partitionAchievements = memoize(completedAchievements =>
+  achievements.reduce(
+    (acc, achievement) => {
+      acc[
+        completedAchievements[achievement.id] ? 'complete' : 'incomplete'
+      ].push(achievement)
+
+      return acc
+    },
+    { complete: [], incomplete: [] }
+  )
+)
+
+const AchievementsList = ({ achievements }) => (
+  <ul className="card-list">
+    {achievements.map(achievement => (
+      <li {...{ key: achievement.id }}>
+        <Achievement {...{ achievement }} />
+      </li>
+    ))}
+  </ul>
+)
+
+const AchievementsView = ({
+  completedAchievements,
+  partitionedAchievements: { complete, incomplete } = partitionAchievements(
+    completedAchievements
+  ),
+}) => (
   <div className="AchievementsView">
-    <ul className="card-list">
-      {achievements.map(achievement => (
-        <li {...{ key: achievement.id }}>
-          <Achievement {...{ achievement }} />
-        </li>
-      ))}
-    </ul>
+    {complete.length ? (
+      <>
+        <h3>Completed</h3>
+        <AchievementsList {...{ achievements: complete }} />
+        <Divider />
+      </>
+    ) : null}
+    <h3>Not Completed</h3>
+    <AchievementsList {...{ achievements: incomplete }} />
   </div>
 )
 
-AchievementsView.propTypes = {}
+AchievementsView.propTypes = {
+  completedAchievements: object.isRequired,
+}
 
 export default function Consumer(props) {
   return (
