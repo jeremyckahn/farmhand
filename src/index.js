ee73fe51a881603eaec3d4342471495427ea89f9
Jeremy Kahn <jeremyckahn@gmail.com>
2020-11-24 20:38:11 -0600

    Add test message

--- src/index.js
+++ src/index.js
@@ -158,3 +158,5 @@ import 'typeface-public-sans'
 window.farmhand = ReactDOM.render(<Farmhand />, document.getElementById('root'))
 
 serviceWorkerRegistration.register()
+
+console.log('test message, please ignore!')
