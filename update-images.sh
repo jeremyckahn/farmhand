69bfad4487d97d22040a8a220b141d25317de4e7
Jeremy Kahn <jeremyckahn@gmail.com>
2020-12-13 22:02:56 -0600

    Remove piskel automation

--- update-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for FILE in `git status --short | grep -o "\S*piskel"`; do
-  DIR=$(echo "$FILE" | grep ".*/" -o)
-  BASENAME=$(basename "$FILE")
-  pushd "$DIR"
-  npx piskel-cli "$BASENAME"
-  popd
-done
