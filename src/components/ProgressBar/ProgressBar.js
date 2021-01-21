aabb97ae83b82190c97ddc314e00d6dbf4e2fa17
Jeremy Kahn <jeremyckahn@gmail.com>
2021-01-20 21:39:16 -0600

    Animate achievement progress bar size
    Animate achievement progress bar color

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,37 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { number } from 'prop-types'
-import { interpolate } from 'shifty'
+import { interpolate, tween } from 'shifty'
 
 import './ProgressBar.sass'
 
 const incompleteColor = '#ff9f00'
 const completeColor = '#00e500'
 
-const ProgressBar = ({
-  percent,
-  fixedPercent = Number(percent.toFixed(2)),
-}) => (
-  <div className="ProgressBar">
-    <div {...{ className: 'progress-wrapper' }}>
-      <div
-        {...{
-          className: 'progress',
-          style: {
-            background: interpolate(
+const ProgressBar = ({ percent }) => {
+  const [displayedProgress, setDisplayedProgress] = useState(0)
+  const [displayedColor, setDisplayedColor] = useState(incompleteColor)
+  const [currentTweenable, setCurrentTweenable] = useState()
+
+  useEffect(() => {
+    if (!currentTweenable) {
+      const tweenable = tween({
+        delay: 750,
+        easing: 'easeInOutQuad',
+        duration: 1500,
+        from: { currentPercent: 0 },
+        to: { currentPercent: percent },
+        render: ({ currentPercent }) => {
+          setDisplayedProgress(currentPercent.toFixed(2))
+          setDisplayedColor(
+            interpolate(
               { color: incompleteColor },
               { color: completeColor },
-              percent / 100
-            ).color,
-            width: `${fixedPercent}%`,
-          },
-        }}
-      ></div>
+              currentPercent / 100
+            ).color
+          )
+        },
+      })
+
+      setCurrentTweenable(tweenable)
+    }
+
+    return () => {
+      if (currentTweenable) {
+        currentTweenable.cancel()
+      }
+    }
+  }, [currentTweenable, percent])
+
+  return (
+    <div className="ProgressBar">
+      <div {...{ className: 'progress-wrapper' }}>
+        <div
+          {...{
+            className: 'progress',
+            style: {
+              background: displayedColor,
+              width: `${displayedProgress}%`,
+            },
+          }}
+        ></div>
+      </div>
+      <p>
+        <span>{displayedProgress}%</span>
+      </p>
     </div>
-    <p>
-      <span>{fixedPercent}%</span>
-    </p>
-  </div>
-)
+  )
+}
 
 ProgressBar.propTypes = {
   percent: number.isRequired,
