0edfbf733eeab63c774172015833daf0ed94ffb8
Jeremy Kahn <jeremyckahn@gmail.com>
2021-03-31 21:23:54 -0500

    Add itch.io's CDN to ACCEPTED_ORIGINS

--- api-etc/constants.js
+++ api-etc/constants.js
@@ -4,5 +4,5 @@ export const ACCEPTED_ORIGINS = new Set([
   'http://localhost:3000',
   'https://farmhand.vercel.app',
   'https://jeremyckahn.github.io',
-  'https://jeremyckahn.itch.io',
+  'https://v6p9d9t4.ssl.hwcdn.net', // itch.io's CDN that the game is served from
 ])
