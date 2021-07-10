42fdb7e0513ac08cbdbc1f33db15d12c5de7da8d
Luke Stebner <luke.stebner@gmail.com>
2021-07-09 23:16:16 -0700

    fixup for specs

--- src/components/Toolbelt/Toolbelt.test.js
+++ src/components/Toolbelt/Toolbelt.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { render, shallow } from 'enzyme'
-import { createMount } from '@material-ui/core/test-utils'
+import { mount, render } from 'enzyme'
 
 import { fieldMode } from '../../enums'
 import tools from '../../data/tools'
@@ -16,20 +15,17 @@ jest.mock('react', () => ({
 }))
 
 describe('<Toolbelt/>', () => {
-  let component, mount
-
-  beforeAll(() => (mount = createMount()))
-  afterAll(() => mount.cleanUp())
+  let component //, mount
 
   describe('render', () => {
     beforeEach(() => {
-      component = mount(
+      component = render(
         <Toolbelt fieldMode={fieldMode.OBSERVE} handleFieldModeSelect={noop} />
       )
     })
 
-    test('renders a Button for every tool', () => {
-      expect(component.find('Button')).toHaveLength(Object.keys(tools).length)
+    test('renders a button for every tool', () => {
+      expect(component.find('button')).toHaveLength(Object.keys(tools).length)
     })
 
     test('does not have any tool selected initially', () => {
@@ -39,21 +35,47 @@ describe('<Toolbelt/>', () => {
 
   describe('tool selection', () => {
     beforeEach(() => {
-      component = mount(
-        <Toolbelt fieldMode={fieldMode.OBSERVE} handleFieldModeSelect={noop} />
-      )
-      console.log(component.debug())
+      class Wrapper extends React.Component {
+        constructor(props) {
+          super(props)
+
+          this.state = {
+            fieldMode: fieldMode.OBSERVE,
+          }
+        }
+
+        handleFieldModeSelect = fieldMode => {
+          this.setState({ fieldMode })
+        }
+
+        render() {
+          return (
+            <Toolbelt
+              fieldMode={this.state.fieldMode}
+              handleFieldModeSelect={this.handleFieldModeSelect}
+            />
+          )
+        }
+      }
+
+      component = mount(<Wrapper />)
     })
 
     describe('a tool is selected', () => {
       test('marks a tool selected when it is clicked', () => {
         component
-          .find('Button')
+          .find('button')
           .first()
           .simulate('click')
-        expect(component.find('.selected').find('.watering-can')).toHaveLength(
-          1
-        )
+        component.update()
+
+        expect(
+          component
+            .find('button')
+            .first()
+            .prop('className')
+            .includes('selected')
+        ).toEqual(true)
       })
     })
   })
