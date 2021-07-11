68d1d09841e1f326f5544bf68e7efedee64bdcbe
Luke Stebner <luke.stebner@gmail.com>
2021-07-10 22:21:20 -0700

    fixup! fixup! chore: adding react-testing-library

--- src/components/Toolbelt/Toolbelt.test.js
+++ src/components/Toolbelt/Toolbelt.test.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 import { fieldMode } from '../../enums'
 
 import { Toolbelt } from './Toolbelt'
-
-const noop = () => {}
-
-let buttons = []
 
 describe('<ToolBelt />', () => {
   const getSelectedButton = () => {
