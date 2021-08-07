77ae4af389a7c6e2b64fd9403e58515c6b5d8755
Jeremy Kahn <jeremyckahn@gmail.com>
2021-08-07 11:04:11 -0500

    [#170] Add more keybinding info

--- src/components/KeybindingsView/KeybindingsView.js
+++ src/components/KeybindingsView/KeybindingsView.js
@@ -21,14 +21,33 @@ const KeybindingsView = () => (
             { label: 'Show Keyboard Shortcuts (this screen)', keybinding: '?' },
             { label: 'Toggle menu', keybinding: 'M' },
             { label: 'End the day', keybinding: 'Shift + C' },
+            { label: 'Go to screen by <1-9> order', keybinding: '<1-9>' },
             { label: 'Go to next screen', keybinding: 'Right arrow key' },
             { label: 'Go to previous screen', keybinding: 'Left arrow key' },
-            { label: 'Go to Bank Account', keybinding: 'B' },
+            { label: 'Go to Farm Stats', keybinding: 'S' },
             { label: 'Go to Achievements', keybinding: 'A' },
             { label: "Go to Farmer's Log", keybinding: 'L' },
             { label: 'Go to Price Events', keybinding: 'E' },
-            { label: 'Go to Farm Stats', keybinding: 'S' },
+            { label: 'Go to Bank Account', keybinding: 'B' },
             { label: 'Go to Settings', keybinding: ',' },
+          ].map(({ label, keybinding }) => (
+            <TableRow {...{ key: label }}>
+              <TableCell {...{ component: 'th', scope: 'row' }}>
+                {label}
+              </TableCell>
+              <TableCell align="right">{keybinding}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <h3>Field-specific keyboard shortcuts</h3>
+    <TableContainer {...{ component: ElevatedPaper }}>
+      <Table aria-label="Keyboard Shortcuts">
+        <TableBody>
+          {[
+            { label: 'Zoom in', keybinding: '=' },
+            { label: 'Zoom out', keybinding: '-' },
           ].map(({ label, keybinding }) => (
             <TableRow {...{ key: label }}>
               <TableCell {...{ component: 'th', scope: 'row' }}>
