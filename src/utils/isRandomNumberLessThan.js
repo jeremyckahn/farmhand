5dbe17915e109aefc2eab763054aecc299cd7070
Luke Stebner <luke.stebner@gmail.com>
2021-09-29 08:54:45 -0700

    fixup! changing isRandomChance to isRandomNumberLessThan

--- src/utils/isRandomNumberLessThan.js
+++ src/utils/isRandomNumberLessThan.js
@@ -1,5 +1,5 @@
 /**
- * Does a dice roll
+ * Compares given number against a randomly generated number
  * @param {number} chance float between 0-1 to compare dice roll against
  * @returns {bool} true if the dice roll was equal to or lower than the given chance, false otherwise
  */
