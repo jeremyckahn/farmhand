bac38ca480981bca5c29948c7215c2dbe7cbbca1
Jeremy Kahn <jeremyckahn@gmail.com>
2022-05-31 08:49:57 -0500

    refactor: move purchaseCow to its own file

--- src/game-logic/reducers/index.js
+++ src/game-logic/reducers/index.js
@@ -7,7 +7,6 @@ import { itemsMap } from '../../data/maps'
 import achievements from '../../data/achievements'
 import {
   areHuggingMachinesInInventory,
-  generateCow,
   getCostOfNextStorageExpansion,
   getCowColorId,
   getCowValue,
@@ -86,37 +85,7 @@ export * from './sellItem'
 export * from './updateLearnedRecipes'
 export * from './makeRecipe'
 export * from './upgradeTool'
-
-/**
- * @param {farmhand.state} state
- * @param {farmhand.cow} cow
- * @returns {farmhand.state}
- */
-export const purchaseCow = (state, cow) => {
-  const { cowInventory, cowColorsPurchased, id, money, purchasedCowPen } = state
-  const { color } = cow
-  const cowValue = getCowValue(cow, false)
-
-  if (
-    money < cowValue ||
-    purchasedCowPen === 0 ||
-    cowInventory.length >= PURCHASEABLE_COW_PENS.get(purchasedCowPen).cows
-  ) {
-    return state
-  }
-
-  state = addCowToInventory(state, { ...cow, ownerId: id, originalOwnerId: id })
-
-  return {
-    ...state,
-    cowColorsPurchased: {
-      ...cowColorsPurchased,
-      [color]: (cowColorsPurchased[color] || 0) + 1,
-    },
-    money: moneyTotal(money, -cowValue),
-    cowForSale: generateCow(),
-  }
-}
+export * from './purchaseCow'
 
 /**
  * @param {farmhand.state} state
