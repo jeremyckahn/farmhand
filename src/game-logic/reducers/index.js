c12b8118b041c2f9130dc728bde7176227169d8a
Jeremy Kahn <jeremyckahn@gmail.com>
2022-05-31 08:35:37 -0500

    refactor: move sellItem to its own file

--- src/game-logic/reducers/index.js
+++ src/game-logic/reducers/index.js
@@ -9,19 +9,11 @@ import upgrades from '../../data/upgrades'
 import {
   areHuggingMachinesInInventory,
   canMakeRecipe,
-  castToMoney,
-  farmProductsSold,
   generateCow,
-  getAdjustedItemValue,
   getCostOfNextStorageExpansion,
   getCowColorId,
   getCowValue,
   getPlotContentType,
-  getResaleValue,
-  getSalePriceMultiplier,
-  isItemAFarmProduct,
-  isItemSoldInShop,
-  levelAchieved,
   moneyTotal,
   nullArray,
 } from '../../utils'
@@ -29,7 +21,6 @@ import {
   COW_GESTATION_PERIOD_DAYS,
   COW_HUG_BENEFIT,
   HUGGING_MACHINE_ITEM_ID,
-  LOAN_GARNISHMENT_RATE,
   MAX_ANIMAL_NAME_LENGTH,
   MAX_DAILY_COW_HUG_BENEFITS,
   MAX_LATEST_PEER_MESSAGES,
@@ -45,7 +36,6 @@ import {
   ACHIEVEMENT_COMPLETED,
   LOAN_INCREASED,
   LOAN_PAYOFF,
-  SOLD_ITEM_PEER_NOTIFICATION,
   TOOL_UPGRADED_NOTIFICATION,
 } from '../../templates'
 import { itemType } from '../../enums'
@@ -54,7 +44,6 @@ import { addItemToInventory } from './addItemToInventory'
 import { decrementItemFromInventory } from './decrementItemFromInventory'
 import { showNotification } from './showNotification'
 import { modifyFieldPlotAt } from './modifyFieldPlotAt'
-import { processLevelUp } from './processLevelUp'
 import { addRevenue } from './addRevenue'
 
 export * from './addItemToInventory'
@@ -95,73 +84,7 @@ export * from './updateInventoryRecordsForNextDay'
 export * from './computeStateForNextDay'
 export * from './purchaseItem'
 export * from './addRevenue'
-
-/**
- * @param {farmhand.state} state
- * @param {farmhand.item} item
- * @param {number} [howMany=1]
- * @returns {farmhand.state}
- */
-export const sellItem = (state, { id }, howMany = 1) => {
-  if (howMany === 0) {
-    return state
-  }
-
-  const item = itemsMap[id]
-  const {
-    completedAchievements,
-    itemsSold,
-    money: initialMoney,
-    valueAdjustments,
-  } = state
-  const oldLevel = levelAchieved(farmProductsSold(itemsSold))
-  let { loanBalance } = state
-
-  const adjustedItemValue = isItemSoldInShop(item)
-    ? getResaleValue(item)
-    : getAdjustedItemValue(valueAdjustments, id)
-  const saleIsGarnished = isItemAFarmProduct(item)
-  let saleValue = 0
-  for (let i = 0; i < howMany; i++) {
-    const loanGarnishment = saleIsGarnished
-      ? Math.min(
-          loanBalance,
-          castToMoney(adjustedItemValue * LOAN_GARNISHMENT_RATE)
-        )
-      : 0
-    const garnishedProfit =
-      adjustedItemValue * getSalePriceMultiplier(completedAchievements) -
-      loanGarnishment
-    loanBalance = moneyTotal(loanBalance, -loanGarnishment)
-    saleValue = moneyTotal(saleValue, garnishedProfit)
-  }
-
-  if (saleIsGarnished) {
-    state = adjustLoan(state, moneyTotal(loanBalance, -state.loanBalance))
-  }
-
-  const newItemsSold = { ...itemsSold, [id]: (itemsSold[id] || 0) + howMany }
-
-  // money needs to be passed in explicitly here because state.money gets
-  // mutated above and addRevenue needs its initial value.
-  state = addRevenue({ ...state, money: initialMoney }, saleValue)
-
-  state = {
-    ...state,
-    itemsSold: newItemsSold,
-  }
-
-  state = processLevelUp(state, oldLevel)
-  state = decrementItemFromInventory(state, id, howMany)
-
-  state = prependPendingPeerMessage(
-    state,
-    SOLD_ITEM_PEER_NOTIFICATION`${howMany}${item}`,
-    'warning'
-  )
-
-  return updateLearnedRecipes(state)
-}
+export * from './sellItem'
 
 /**
  * @param {farmhand.state} state
---
+++ src/game-logic/reducers/sellItem.test.js
@@ -0,0 +1,142 @@
+import { testItem } from '../../test-utils'
+import { LOAN_PAYOFF } from '../../templates'
+
+import { sellItem } from './sellItem'
+
+jest.mock('../../data/maps')
+
+describe('sellItem', () => {
+  test('sells item', () => {
+    const state = sellItem(
+      {
+        inventory: [testItem({ id: 'sample-item-1', quantity: 1 })],
+        itemsSold: {},
+        loanBalance: 0,
+        money: 100,
+        pendingPeerMessages: [],
+        todaysNotifications: [],
+        revenue: 0,
+        todaysRevenue: 0,
+        valueAdjustments: { 'sample-item-1': 1 },
+      },
+      testItem({ id: 'sample-item-1' })
+    )
+
+    expect(state.inventory).toEqual([])
+    expect(state.money).toEqual(101)
+    expect(state.revenue).toEqual(1)
+    expect(state.todaysRevenue).toEqual(1)
+    expect(state.itemsSold).toEqual({ 'sample-item-1': 1 })
+  })
+
+  test('updates learnedRecipes', () => {
+    const { learnedRecipes } = sellItem(
+      {
+        inventory: [testItem({ id: 'sample-item-1', quantity: 3 })],
+        itemsSold: {},
+        loanBalance: 0,
+        money: 100,
+        pendingPeerMessages: [],
+        todaysNotifications: [],
+        revenue: 0,
+        todaysRevenue: 0,
+        valueAdjustments: { 'sample-item-1': 1 },
+      },
+      testItem({ id: 'sample-item-1' }),
+      3
+    )
+
+    expect(learnedRecipes['sample-recipe-1']).toBeTruthy()
+  })
+
+  describe('there is an outstanding loan', () => {
+    let state
+
+    describe('item is not a farm product', () => {
+      test('sale is not garnished', () => {
+        state = sellItem(
+          {
+            inventory: [testItem({ id: 'sample-crop-seeds-1', quantity: 3 })],
+            itemsSold: {},
+            loanBalance: 100,
+            money: 100,
+            pendingPeerMessages: [],
+            todaysNotifications: [],
+            revenue: 0,
+            todaysRevenue: 0,
+            valueAdjustments: { 'sample-crop-seeds-1': 10 },
+          },
+          testItem({ id: 'sample-crop-seeds-1' }),
+          3
+        )
+
+        expect(state.loanBalance).toEqual(100)
+        expect(state.money).toEqual(130)
+        expect(state.revenue).toEqual(30)
+        expect(state.todaysRevenue).toEqual(30)
+      })
+    })
+
+    describe('item is a farm product', () => {
+      describe('loan is greater than garnishment', () => {
+        test('sale is garnished', () => {
+          state = sellItem(
+            {
+              inventory: [testItem({ id: 'sample-crop-1', quantity: 3 })],
+              itemsSold: {},
+              loanBalance: 100,
+              money: 100,
+              pendingPeerMessages: [],
+              todaysNotifications: [],
+              revenue: 0,
+              todaysRevenue: 0,
+              valueAdjustments: { 'sample-crop-1': 10 },
+            },
+            testItem({ id: 'sample-crop-1' }),
+            3
+          )
+
+          expect(state.loanBalance).toEqual(97)
+          expect(state.money).toEqual(157)
+          expect(state.revenue).toEqual(57)
+          expect(state.todaysRevenue).toEqual(57)
+        })
+      })
+
+      describe('loan is less than garnishment', () => {
+        beforeEach(() => {
+          state = sellItem(
+            {
+              inventory: [testItem({ id: 'sample-crop-1', quantity: 3 })],
+              itemsSold: {},
+              loanBalance: 1.5,
+              money: 100,
+              pendingPeerMessages: [],
+              todaysNotifications: [],
+              revenue: 0,
+              todaysRevenue: 0,
+              valueAdjustments: { 'sample-crop-1': 10 },
+            },
+            testItem({ id: 'sample-crop-1' }),
+            3
+          )
+        })
+
+        test('loan is payed off', () => {
+          expect(state.loanBalance).toEqual(0)
+        })
+
+        test('sale is reduced based on remaining loan balance', () => {
+          expect(state.money).toEqual(158.5)
+          expect(state.todaysRevenue).toEqual(58.5)
+        })
+
+        test('payoff notification is shown', () => {
+          expect(state.todaysNotifications).toEqual([
+            { message: LOAN_PAYOFF``, severity: 'success' },
+          ])
+        })
+      })
+    })
+  })
+})
