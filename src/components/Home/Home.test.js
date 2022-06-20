895a6a5de13564fe6100f3c4516d7afc070ff39b
Luke Stebner <luke.stebner@gmail.com>
2022-06-20 10:03:08 -0700

    add tests for go to shop button

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,28 +1,45 @@
 import React from 'react'
 
 import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import FarmhandContext from '../Farmhand/Farmhand.context'
 
 import Home from './Home'
 
-beforeEach(() => {
-  const gameState = {
-    completedAchievements: {},
-  }
+describe('<Home />', () => {
+  let handleViewChangeButtonClick
 
-  render(
-    <FarmhandContext.Provider value={{ gameState, handlers: {} }}>
-      <Home
-        {...{
-          completedAchievements: {},
-          handleViewChangeButtonClick: () => {},
-        }}
-      />
-    </FarmhandContext.Provider>
-  )
-})
+  beforeEach(() => {
+    const gameState = {
+      completedAchievements: {},
+    }
 
-test('renders', () => {
-  expect(screen.getByText('Welcome!')).toBeInTheDocument()
+    handleViewChangeButtonClick = jest.fn()
+
+    render(
+      <FarmhandContext.Provider value={{ gameState, handlers: {} }}>
+        <Home handleViewChangeButtonClick={handleViewChangeButtonClick} />
+      </FarmhandContext.Provider>
+    )
+  })
+
+  test('rendered welcome message', () => {
+    expect(screen.getByText('Welcome!')).toBeInTheDocument()
+  })
+
+  describe('go to shop', () => {
+    const shopButton = () =>
+      screen.getByRole('button', { name: 'Go to the shop' })
+
+    test('contains an action to go to the shop', () => {
+      expect(shopButton()).toBeInTheDocument()
+    })
+
+    test('calls to change view when shop button is clicked', () => {
+      userEvent.click(shopButton())
+
+      expect(handleViewChangeButtonClick).toHaveBeenCalledWith('SHOP')
+    })
+  })
 })
