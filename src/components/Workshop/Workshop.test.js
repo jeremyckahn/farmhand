d9a94cd78602b7bc1b94eba4bbb3f4b442f12ccd
Luke Stebner <luke.stebner@gmail.com>
2022-06-20 10:50:43 -0700

    wip: update Workshop to use rtl
        - add some missing props to propTypes
        - convert existing test
        - started to add some more coverage

--- src/components/Workshop/Workshop.test.js
+++ src/components/Workshop/Workshop.test.js
@@ -1,20 +1,72 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { screen, render } from '@testing-library/react'
+
+import FarmhandContext from '../Farmhand/Farmhand.context'
 
 import Workshop from './Workshop'
 
-let component
+jest.mock('../../data/maps', () => ({
+  ...jest.requireActual('../../data/maps'),
+  recipesMap: {
+    'kitchen-recipe-1': {
+      id: 'kitchen-recipe-1',
+      name: 'kitchen recipe 1',
+      recipeType: 'KITCHEN',
+    },
+  },
+}))
 
-beforeEach(() => {
-  component = shallow(
-    <Workshop
-      {...{
-        learnedRecipes: {},
-      }}
-    />
-  )
-})
+jest.mock('../Recipe', () => () => 'Recipe')
 
-test('renders', () => {
-  expect(component).toHaveLength(1)
+describe('<Workshop />', () => {
+  let gameState = {
+    learnedRecipes: {},
+    purchasedSmelter: false,
+    toolLevels: {},
+  }
+
+  const renderWorkshop = gameState => {
+    render(
+      <FarmhandContext.Provider value={{ gameState, handlers: {} }}>
+        <Workshop />
+      </FarmhandContext.Provider>
+    )
+  }
+
+  describe('kitchen', () => {
+    test('is available', () => {
+      renderWorkshop(gameState)
+
+      expect(screen.getByText('Kitchen')).toBeInTheDocument()
+    })
+
+    test('renders learned recipes', () => {
+      gameState.learnedRecipes = {
+        'kitchen-recipe-1': true,
+      }
+      renderWorkshop(gameState)
+
+      expect(
+        screen.getByText('Learned Recipes (1', { exact: false })
+      ).toBeInTheDocument()
+    })
+  })
+
+  describe('forge', () => {
+    test('is rendered if smelter is purchased', () => {
+      gameState.purchasedSmelter = true
+
+      renderWorkshop(gameState)
+
+      expect(screen.getByText('Forge')).toBeInTheDocument()
+    })
+
+    test('is not rendered if smelter is not purchased', () => {
+      gameState.purchasedSmelter = false
+
+      renderWorkshop(gameState)
+
+      expect(screen.queryByText('Forge')).not.toBeInTheDocument()
+    })
+  })
 })
