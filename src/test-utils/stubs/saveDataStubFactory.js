172722ad9373ee48a71f1fe88d9960f0896a20d7
Luke Stebner <luke.stebner@gmail.com>
2022-07-17 19:03:14 -0700

    add field to saveDataStubFactory

--- src/test-utils/stubs/saveDataStubFactory.js
+++ src/test-utils/stubs/saveDataStubFactory.js
@@ -4,7 +4,12 @@ export const saveDataStubFactory = ({ dayCount = 1, ...restOverrides }) =>
   computeStateForNextDay(
     // dayCount is offset by 1 here to account for the fact that
     // computeStateForNextDay's isFirstDay argument below is true.
-    { dayCount: dayCount - 1, valueAdjustments: {}, ...restOverrides },
+    {
+      dayCount: dayCount - 1,
+      valueAdjustments: {},
+      field: [[]],
+      ...restOverrides,
+    },
 
     // Necessary for the stubbed state to be valid.
     true
