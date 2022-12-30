9b40201529da9a7b7fc581c804b5caee4f9e54da
Jeremy Kahn <me@jeremyckahn.com>
2022-12-30 15:59:10 -0600

    chore: Use Node 18 (#360)
    * chore: use node 18
    
    - Required upgrade to node-sass 8.0.0
    - Required --openssl-legacy-provider
      - See: https://nodejs.org/en/blog/release/v17.0.0/#openssl-3-0
      - See: https://stackoverflow.com/a/69699772
      - See: https://github.blog/changelog/2022-09-22-github-actions-all-actions-will-begin-running-on-node16-instead-of-node12/
    
    * fix: use --openssl-legacy-provider for build
    
    * fix: update to non-deprecated MUI APIs
    
    - See: https://github.com/actions/upload-artifact/issues/351
    
    - See: https://github.com/dorny/test-reporter/issues/219
    
    * chore: unpin GitHub action versions

--- src/mui-theme.js
+++ src/mui-theme.js
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core'
+import { createTheme } from '@material-ui/core'
 
-export default createMuiTheme({
+export default createTheme({
   typography: {
     // Needed to silence this warning:
     // https://material-ui.com/style/typography/#migration-to-typography-v2
