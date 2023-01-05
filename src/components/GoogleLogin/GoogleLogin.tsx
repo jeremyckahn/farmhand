ab25cbbbb0e47b8485b176a7f33517c438af223c
Jeremy Kahn <jeremyckahn@gmail.com>
2023-01-04 21:35:17 -0600

    feat: [#213] [wip] attempt to set up Google API signin

--- src/components/GoogleLogin/GoogleLogin.tsx
+++ src/components/GoogleLogin/GoogleLogin.tsx
@@ -17,7 +17,47 @@ export const GoogleLogin = () => {
     })()
   }, [hasLoadedGapi])
 
-  const handleEnableCloudSaveClick = () => {}
+  const handleEnableCloudSaveClick = () => {
+    // https://medium.com/@willikay11/how-to-link-your-react-application-with-google-drive-api-v3-list-and-search-files-2e4e036291b7
+    const { gapi } = window
+
+    // Array of API discovery doc URLs for APIs
+    const DISCOVERY_DOCS = [
+      'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+    ]
+
+    // Authorization scopes required by the API; multiple scopes can be
+    // included, separated by spaces.
+    const SCOPES = 'https://www.googleapis.com/auth/drive.appdata'
+
+    const initClient = async () => {
+      // setIsLoadingGoogleDriveApi(true)
+
+      try {
+        await gapi.client.init({
+          apiKey: process.env.REACT_APP_GOOGLE_DRIVE_API_KEY,
+          clientId: process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES,
+        })
+      } catch (e) {
+        console.error(e)
+        return
+      }
+
+      console.log(gapi)
+
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(isSignedIn => {
+        console.log({ isSignedIn })
+      })
+
+      // Handle the initial sign-in state.
+      // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    }
+
+    gapi.load('client:auth2', initClient)
+  }
 
   return hasLoadedGapi ? (
     <Button
