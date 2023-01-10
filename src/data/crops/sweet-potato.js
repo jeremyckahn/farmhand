873c58917f8a4aeb4d665a304a1ac8b6e5a1810e
Luke Stebner <luke.stebner@gmail.com>
2023-01-09 23:14:38 -0800

    fixup! feat: [closes #369] add sweet potato

---
+++ src/data/crops/sweet-potato.js
@@ -0,0 +1,29 @@
+import { crop, fromSeed } from '../crop'
+import { cropLifeStage, cropType } from '../../enums'
+
+const { SEED, GROWING } = cropLifeStage
+
+/**
+ * @property farmhand.module:items.sweetPotatoSeed
+ * @type {farmhand.item}
+ */
+export const sweetPotatoSeed = crop({
+  cropType: cropType.SWEET_POTATO,
+  cropTimetable: {
+    [SEED]: 2,
+    [GROWING]: 6,
+  },
+  growsInto: 'sweet-potato',
+  id: 'sweet-potato-seed',
+  name: 'Sweet Potato Slip',
+  tier: 6,
+})
+
+/**
+ * @property farmhand.module:items.sweetPotato
+ * @type {farmhand.item}
+ */
+export const sweetPotato = crop({
+  ...fromSeed(sweetPotatoSeed),
+  name: 'Sweet Potato',
+})
