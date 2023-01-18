80803b929168f6b46fd619cac5921c62f3b87855
Jeremy Kahn <jeremyckahn@gmail.com>
2023-01-17 21:38:02 -0600

    feat: define grape items

---
+++ src/data/crops/grape.js
@@ -0,0 +1,49 @@
+/** @typedef {import("../../index").farmhand.item} farmhand.item */
+
+import { crop, fromSeed } from '../crop'
+import { cropLifeStage, cropType } from '../../enums'
+
+const { SEED, GROWING } = cropLifeStage
+
+/**
+ * @property farmhand.module:items.grapeSeed
+ * @type {farmhand.item}
+ */
+export const grapeSeed = crop({
+  cropType: cropType.GRAPE,
+  cropTimetable: {
+    [SEED]: 3,
+    [GROWING]: 4,
+  },
+  growsInto: ['grape-green', 'grape-purple', 'grape-red'],
+  id: 'grape-seed',
+  name: 'Grape Bulb',
+  tier: 7,
+})
+
+/**
+ * @property farmhand.module:items.grapeGreen
+ * @type {farmhand.item}
+ */
+export const grapeGreen = crop({
+  ...fromSeed(grapeSeed, 0),
+  name: 'Green Grape',
+})
+
+/**
+ * @property farmhand.module:items.grapePurple
+ * @type {farmhand.item}
+ */
+export const grapePurple = crop({
+  ...fromSeed(grapeSeed, 1),
+  name: 'Purple Grape',
+})
+
+/**
+ * @property farmhand.module:items.grapeRed
+ * @type {farmhand.item}
+ */
+export const grapeRed = crop({
+  ...fromSeed(grapeSeed, 2),
+  name: 'Red Grape',
+})
