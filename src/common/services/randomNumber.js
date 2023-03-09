85343aa701a3b1c502da2f0ba3be1d31e22d8824
Jeremy Kahn <jeremyckahn@gmail.com>
2023-03-08 20:48:40 -0600

    feat: implement number seeding in RandomNumberService

--- src/common/services/randomNumber.js
+++ src/common/services/randomNumber.js
@@ -1,6 +1,20 @@
+import seedrandom from 'seedrandom'
+
 export class RandomNumberService {
+  random = Math.random()
+
+  /**
+   * @param {string} seed
+   */
+  seedRandomNumber(seed) {
+    this.random = seedrandom(seed)
+  }
+
+  /**
+   * @returns {number}
+   */
   generateRandomNumber() {
-    return Math.random()
+    return this.random()
   }
 }
 
