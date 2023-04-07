ee5a03a1369d16fd524271b2331aee69ffcb270f
Jeremy Kahn <jeremyckahn@gmail.com>
2023-04-07 09:46:51 -0500

    docs: relax TODO to notice

--- src/data/items-map.js
+++ src/data/items-map.js
@@ -2,10 +2,9 @@
 
 import * as items from '../data/items'
 
-// TODO: This object is unsafe to use directly. Move it to a private variable
-// in src/data/maps.js.
-
 /**
+ * ⚠️ This is a low-level object that is UNSAFE to use directly outside of
+ * initial bootup. Use itemsMap in src/data/maps.js instead.
  * @type {Object.<string, farmhand.item>}
  */
 export default {
