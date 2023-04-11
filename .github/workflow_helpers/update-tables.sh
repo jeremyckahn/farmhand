f0eeef91bd2cd2f6e6d604e4880a2ac567624296
Jeremy Kahn <me@jeremyckahn.com>
2023-04-11 12:16:43 -0500

    feat: Automatically update wiki tables (#406)
    * feat: set up update-wiki.yml
    * fix: only push table updates if a change was made
    * feat: print message when wiki update is skipped

---
+++ .github/workflow_helpers/update-tables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://spinscale.de/posts/2021-10-29-github-actions-updating-github-wiki-after-edit.html
+
+#########
+# some debug output to check in case of failures
+# also exit on any error
+set -xe
+
+cd $1
+
+npm run --silent print:crops > Crops.md
+
+echo "Updated Crops.md"
+
+if [[ `git status --short | wc -l` > 0 ]]; then
+  git config --global user.email "github-actions@users.noreply.github.com"
+  git config --global user.name "github-actions[bot]"
+  git add Crops.md
+  git commit -m "Update tables"
+  git push origin master
+else
+  echo "No table changes were made. Skipping wiki update."
+fi
