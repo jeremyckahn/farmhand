cb1e5cff32f5846a370c1328b3d1d1687d08e390
Jeremy Kahn <jeremyckahn@gmail.com>
2023-04-11 09:19:55 -0500

    feat: [wip] set up update-wiki.yml

---
+++ .github/workflow_helpers/update-tables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# https://spinscale.de/posts/2021-10-29-github-actions-updating-github-wiki-after-edit.html
+
+#########
+# some debug output to check in case of failures
+# also exit on any error
+set -xe
+
+cd $1
+
+npm run --silent print:crops > Crops.md
+
+echo "Updated Crops.md"
+
+git config --global user.email "github-actions@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+git add Crops.md
+git commit -m "Update tables"
+git push origin master
