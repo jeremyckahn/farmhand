6d12e51f6acaea8d9c3be9e152fa0be67988a9d7
Jeremy Kahn <jeremyckahn@gmail.com>
2023-05-04 09:48:06 -0500

    feat(FermentationRecipeList): [wip] add FermentationRecipe card list

--- src/components/FermentationRecipeList/FermentationRecipeList.js
+++ src/components/FermentationRecipeList/FermentationRecipeList.js
@@ -1,8 +1,16 @@
 import React, { useContext } from 'react'
 
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+
 import { itemsMap, fermentableItemsMap } from '../../data/maps'
 import FarmhandContext from '../Farmhand/Farmhand.context'
 import { getFinalCropItemFromSeedItem } from '../../utils'
+import { items } from '../../img'
+
+import './FermentationRecipe.sass'
 
 const totalFermentableItems = Object.keys(fermentableItemsMap).length
 
@@ -18,13 +26,25 @@ export function FermentationRecipeList() {
   const numberOfCropsAvailableToFerment = Object.keys(cropsAvailableToFerment)
     .length
 
+  // FIXME: Test recipe count display
   return (
     <>
       <h3>
         Learned Fermentation Recipes ({numberOfCropsAvailableToFerment} /{' '}
         {totalFermentableItems})
       </h3>
-      <ul className="card-list"></ul>
+      <ul className="card-list">
+        {cropsAvailableToFerment.map(item => (
+          <Card key={item.id} className="FermentationRecipe">
+            <CardHeader
+              title={item.name}
+              avatar={<img {...{ src: items[item.id] }} alt={item.name} />}
+            ></CardHeader>
+            <CardContent></CardContent>
+            <CardActions></CardActions>
+          </Card>
+        ))}
+      </ul>
     </>
   )
 }
