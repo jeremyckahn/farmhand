2d2ef9513b8a8aaa4254427ea67bec739ddeb372
Jeremy Kahn <jeremyckahn@gmail.com>
2023-05-05 09:19:20 -0500

    feat(FermentationRecipe): show fermented recipe name

--- src/components/FermentationRecipeList/FermentationRecipe.js
+++ src/components/FermentationRecipeList/FermentationRecipe.js
@@ -5,25 +5,31 @@ import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 
+import { items } from '../../img'
+
 import './FermentationRecipe.sass'
 
-export const FermentationRecipe = ({ item }) => (
-  <Card className="FermentationRecipe">
-    <CardHeader
-      title={item.name}
-      avatar={
-        <img
-          {...{
-            src: item[item.id],
-          }}
-          alt={item.name}
-        />
-      }
-    ></CardHeader>
-    <CardContent></CardContent>
-    <CardActions></CardActions>
-  </Card>
-)
+export const FermentationRecipe = ({ item }) => {
+  const fermentationRecipeName = `Fermented ${item.name}`
+
+  return (
+    <Card className="FermentationRecipe">
+      <CardHeader
+        title={fermentationRecipeName}
+        avatar={
+          <img
+            {...{
+              src: items[item.id],
+            }}
+            alt={fermentationRecipeName}
+          />
+        }
+      ></CardHeader>
+      <CardContent></CardContent>
+      <CardActions></CardActions>
+    </Card>
+  )
+}
 
 FermentationRecipe.propTypes = {
   item: object.isRequired,
