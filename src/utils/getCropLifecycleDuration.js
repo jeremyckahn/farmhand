c4d5b2301ef7a2f7de3c3e5be5d0a7ab2220c4c5
Jeremy Kahn <jeremyckahn@gmail.com>
2023-05-22 09:22:17 -0500

    chore(todos): add TODO links

--- src/utils/getCropLifecycleDuration.js
+++ src/utils/getCropLifecycleDuration.js
@@ -1,6 +1,7 @@
 import { memoize } from './memoize'
 
 // TODO: Refactor this to accept just a plain cropTimetable
+// https://github.com/jeremyckahn/farmhand/issues/415
 /**
  * @param {{ cropTimetable: Object }} crop
  * @returns {number}
