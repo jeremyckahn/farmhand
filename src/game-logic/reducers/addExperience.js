89bb2ee6e8158ddaa29afda3b234bd022bd7d163
Luke Stebner <luke.stebner@gmail.com>
2023-08-28 09:05:29 -0700

    remove processLevelUp from addExperience

--- src/game-logic/reducers/addExperience.js
+++ src/game-logic/reducers/addExperience.js
@@ -1,20 +1,11 @@
-import { levelAchieved } from '../../utils/levelAchieved'
-
-import { processLevelUp } from './processLevelUp'
-
 /**
  * @param {farmhand.state} state
  * @param {number} amount
  * @returns {farmhand.state}
  */
 export const addExperience = (state, amount) => {
-  const newExperience = state.experience + amount
-  const oldLevel = levelAchieved({ itemsSold: state.itemsSold })
-
-  state = processLevelUp(state, oldLevel)
-
   return {
     ...state,
-    experience: newExperience,
+    experience: state.experience + amount,
   }
 }
