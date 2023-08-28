fd88b395cd0154059c2dfa2156ea5caa397593ac
Luke Stebner <luke.stebner@gmail.com>
2023-08-27 17:33:41 -0700

    add jsdoc to addExperience

--- src/game-logic/reducers/addExperience.js
+++ src/game-logic/reducers/addExperience.js
@@ -2,6 +2,11 @@ import { levelAchieved } from '../../utils/levelAchieved'
 
 import { processLevelUp } from './processLevelUp'
 
+/**
+ * @param {farmhand.state} state
+ * @param {number} amount
+ * @returns {farmhand.state}
+ */
 export const addExperience = (state, amount) => {
   const newExperience = state.experience + amount
   const oldLevel = levelAchieved({ itemsSold: state.itemsSold })
