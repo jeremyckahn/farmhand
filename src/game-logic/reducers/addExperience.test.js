2ce12be9cf485e3270cc2df2b8f7bc738b552131
Luke Stebner <luke.stebner@gmail.com>
2023-09-03 19:40:36 -0700

    add processLevelUp to addExperience

--- src/game-logic/reducers/addExperience.test.js
+++ src/game-logic/reducers/addExperience.test.js
@@ -1,11 +1,32 @@
+import { experienceNeededForLevel } from '../../utils'
+
 import { addExperience } from './addExperience'
 
 describe('addExperience', () => {
+  let gameState
+
+  beforeEach(() => {
+    gameState = {
+      experience: 0,
+      inventory: [],
+      showNotifications: true,
+      todaysNotifications: [],
+    }
+  })
+
   it('adds experience to current experience', () => {
-    const gameState = { experience: 0, itemsSold: {} }
+    const newState = addExperience(gameState, 10)
 
-    const newState = addExperience(gameState, 100)
+    expect(newState.experience).toEqual(10)
+  })
 
-    expect(newState.experience).toEqual(100)
+  it('process a level up when enough experience is achieved', () => {
+    const newState = addExperience(gameState, experienceNeededForLevel(2))
+
+    const notification = /You reached \*\*level 2!\*\*/
+
+    expect(notification.test(newState.todaysNotifications[0].message)).toEqual(
+      true
+    )
   })
 })
