936d683a88f757ca6de1df8def7e5a57f2e5a6b7
Jeremy Kahn <me@jeremyckahn.com>
2024-05-07 20:31:00 -0500

    feat(#491): show wine instances in cellar

--- src/components/WineRecipeList/WineRecipe.js
+++ src/components/WineRecipeList/WineRecipe.js
@@ -16,6 +16,7 @@ import { integerString } from '../../utils'
 import { getInventoryQuantityMap } from '../../utils/getInventoryQuantityMap'
 import FarmhandContext from '../Farmhand/Farmhand.context'
 import { GRAPES_REQUIRED_FOR_WINE } from '../../constants'
+import { cellarService } from '../../services/cellar'
 
 /**
  * @param {{
@@ -24,12 +25,17 @@ import { GRAPES_REQUIRED_FOR_WINE } from '../../constants'
  */
 export const WineRecipe = ({ wineVariety }) => {
   const {
-    gameState: { inventory },
+    gameState: { cellarInventory, inventory },
   } = useContext(FarmhandContext)
   const wineName = grapeVarietyNameMap[wineVariety]
   const grape = grapeVarietyVarietyGrapeMap[wineVariety]
 
   const inventoryQuantityMap = getInventoryQuantityMap(inventory)
+
+  const wineInstancesInCellar = cellarService.getItemInstancesInCellar(
+    cellarInventory,
+    grape
+  )
 
   return (
     <Card className="WineRecipe">
@@ -45,22 +51,32 @@ export const WineRecipe = ({ wineVariety }) => {
         }
         subheader={
           <>
+            {
+              // FIXME: Test this
+            }
             <p>
               Days to mature:{' '}
               {integerString(wineService.getDaysToMature(wineVariety))}
             </p>
+            {
+              // FIXME: Test this
+            }
             <p>
               Units of {grape.name} required:{' '}
               {integerString(GRAPES_REQUIRED_FOR_WINE)} (available:{' '}
               {integerString(inventoryQuantityMap[grape.id])})
             </p>
+            {
+              // FIXME: Test this
+            }
             <p>
               Units of {'yeast'} required:{' '}
               {integerString(wineService.getYeastRequiredForWine(wineVariety))}
             </p>
             {
-              // FIXME: Show amount in cellar
+              // FIXME: Test this
             }
+            <p>In cellar: {integerString(wineInstancesInCellar ?? 0)}</p>
           </>
         }
       ></CardHeader>
