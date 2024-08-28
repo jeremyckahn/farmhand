294092cc7aa7bc74dec313db4d266c5ac0a18751
Jeremy Kahn <me@jeremyckahn.com>
2024-08-28 08:50:10 -0500

    chore(deps): remove unused jest packages

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,14 +2,10 @@ import '@testing-library/jest-dom'
 import 'jest-extended'
 import Adapter from 'enzyme-adapter-react-16'
 import { configure } from 'enzyme'
-import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
-
-expect.extend(matchers)
 
 configure({
   adapter: new Adapter(),
-  setupFilesAfterEnv: ['jest-extended'],
 })
 
 beforeEach(() => {
