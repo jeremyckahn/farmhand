fb76700041d4288a232b396dcb0ac27549ed18a2
Jeremy Kahn <me@jeremyckahn.com>
2025-10-01 08:23:44 -0500

    fix(e2e): Use consistent date for E2E tests (#573)
    * fix(e2e): use consistent date for E2E tests

--- e2e/test-utils/open-page.js
+++ e2e/test-utils/open-page.js
@@ -1,10 +1,16 @@
+const gameStartTime = '2025-01-01T09:00:00'
+
 /**
  * @param {import('@playwright/test').Page} page
  * @param {number=} seed
  * @returns {Promise<import('@playwright/test').Response | null>}
  */
-export const openPage = (page, seed = 0.5) => {
+export const openPage = async (page, seed = 0.5) => {
   const appUrl = process.env.APP_URL || 'http://localhost:3000'
+
+  // NOTE: A consistent date for the game is set so that time-based events
+  // don't interfere with the tests
+  await page.clock.install({ time: gameStartTime })
 
   return page.goto(`${appUrl}?seed=${seed}`)
 }
