3b3af7973145185ed2c7b60c6f57eebd6e9eb2b0
mariatedeman <m.tedeman@hotmail.com>
2026-09-23 16:11:26 +0200

    Updated blueberry access to level 46

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -35,10 +35,6 @@ levels[6] = {
   unlocksTool: [toolType.SHOVEL],
 }
 
-levels[7] = {
-  unlocksShopItem: items.blueberrySeed.id,
-}
-
 levels[8] = {
   increasesSprinklerRange: true,
 }
@@ -127,6 +123,10 @@ levels[42] = {
 
 levels[44] = {
   unlocksShopItem: items.sugarcaneSeed.id,
+}
+
+levels[46] = {
+  unlocksShopItem: items.blueberrySeed.id,
 }
 
 for (let i = 0; i < levels.length; i++) {
